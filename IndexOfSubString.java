5dabba73717c0ef65e0258b6983c91cae3e1190f
kumareshsaran <sarankumaresh1@gmail.com>
2016-09-10 12:08:52 +0530

    Create IndexOfSubString.java

---
+++ IndexOfSubString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SubStringindex  {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String a=scan.nextLine();
+		String b=scan.nextLine();
+		System.out.println(a.indexOf(b));
+	}
+
+}
